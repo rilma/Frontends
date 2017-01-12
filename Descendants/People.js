14eb6d954eba45071f18b57223538f7a722f7795
Ronald Ilma <rri5@cornell.edu>
2017-01-12 12:54:10 +0000

    Add files via upload

---
+++ Descendants/People.js
@@ -0,0 +1,62 @@
+angular.module('PeopleApp', ['ngMaterial'])
+    .controller('PeopleController', function($scope) {
+        $scope.people = [
+            {
+                name: "Alice",
+                children: ["Bob"]
+            },
+            {
+                name: "Bob",
+                children: ["Chloe", "John"]
+            },
+            {
+                name: "Chloe",
+                children: []
+            },
+            {
+                name: "John",
+                children: []
+            }
+        ]
+
+        $scope.findDescendants = function (parent, people) {
+            // TODO return an array with the names of all descendants of "parent" in "people"
+            // return [];
+/*
+            var descendants = [];
+
+            descendants.push(parent.children.join(', '));
+
+            for (var i = 0; i < parent.children.length; i++) {
+                for (var j = 0; j < people.length; j++) {
+                    if (parent.children[i] == people[j].name) {
+                        for (var k = 0; k < people[j].children.length; k++) descendants.push(people[j].children[k]);
+                    }
+                }
+            }
+            return [descendants.join(', ')];
+*/
+
+            var descendants = [];
+            descendants.push(parent.children.join(', '));
+            for (var i = 0; i < parent.children.length; i++) {
+                descendants.push(findDescendants(parent.children[i], people));
+            }
+            return [descendants.join(', ')];
+        }
+
+        $scope.person = {name: '', children: []};
+
+        $scope.addPerson = function () {
+            var oldIndex = $scope.people.find(function (person) {
+                return person.name == $scope.person.name;
+            });
+
+            if(oldIndex) {
+                $scope.people.splice(oldIndex, 1);
+            }
+
+            $scope.people.push($scope.person);
+            $scope.person = {name: '', children: []};
+        }
+    });
