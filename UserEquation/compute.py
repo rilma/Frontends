c8869f416adb07e78e006202eb97d7030ca2d550
Ronald Ilma <rri5@cornell.edu>
2017-01-20 16:55:49 -0500

    Add files via upload

--- UserEquation/compute.py
+++ UserEquation/compute.py
@@ -1,10 +1,12 @@
+from io import BytesIO
 from matplotlib.pyplot import figure, savefig
 from scipy import linspace, pi
-import glob, os, scipy, seaborn, time
+import base64, glob, os, scipy, seaborn, time
 
 FILE_TYPE = 'png'
 
 def compute(f, xlim):
+    
     fig = figure(figsize=(8,6))
     pn = fig.add_subplot(111)    
     namespace = scipy.__dict__.copy()
@@ -12,10 +14,14 @@ def compute(f, xlim):
     namespace.update({'x': x})
     y = eval(f, namespace)
     pn.plot(x, y)
-    from io import BytesIO
+    
+    # Creating the object to be embedded into the HTML template
+    #
     pfile = BytesIO()
     savefig(pfile, format=FILE_TYPE)
     pfile.seek(0)
-    import base64
     pfilePNG = base64.b64encode(pfile.getvalue())
-    return pfilePNG
+
+    # Turning to Python string and return
+    #
+    return pfilePNG.decode('utf-8').rstrip('\n')
